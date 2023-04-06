a878f4a9fcb383a5cdf1bb0380993c8e8339cbe2
CBreezyJR <kazzyjr123@gmail.com>
2023-04-06 08:33:07 +0530

    beno

---
+++ choose.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Choose Interest</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+
+
+<form>
+ 
+
+<div class="container text-center">
+  <div class="row justify-content-start">
+    <div class="col-4">
+      One of two columns
+    </div>
+    <div class="col-4">
+      One of two columns
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-4">
+      One of two columns
+    </div>
+    <div class="col-4">
+      One of two columns
+    </div>
+  </div>
+  <div class="row justify-content-end">
+    <div class="col-4">
+    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+     <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+      <label class="btn btn-outline-primary" for="btncheck1">#Cryptocurrency</label>
+    </div>
+    <div class="col-4">
+    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+      <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+      <label class="btn btn-outline-primary" for="btncheck1">#Technology</label>
+    </div>
+  </div>
+  <div class="row justify-content-around">
+    <div class="col-4">
+    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+     <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+      <label class="btn btn-outline-primary" for="btncheck1">#Sales</label>
+    </div>
+    <div class="col-4">
+    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+     <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+      <label class="btn btn-outline-primary" for="btncheck1">#Social Media</label>
+    </div>
+  </div>
+  <div class="row justify-content-between">
+    <div class="col-4">
+    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+     <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+      <label class="btn btn-outline-primary" for="btncheck1">#Branding</label>
+    </div>
+    <div class="col-4">
+      One of two columns
+    </div>
+  </div>
+  <div class="row justify-content-evenly">
+    <div class="col-4">
+      One of two columns
+    </div>
+    <div class="col-4">
+      One of two columns
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+</html>
---
+++ sign up.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sign Up</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+
+
+
+
+
+
+<form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div><br>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div><br>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <label for="inputName" class="form-label">First Name</label>
+    <input type="text" class="form-control" id="inputName" placeholder="First Name">
+  </div>
+  <div class="col-12">
+    <label for="inputName" class="form-label">Last Name</label>
+    <input type="text" class="form-control" id="inputName" placeholder="Last Name">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCompany" class="form-label">Company</label>
+    <input type="text" class="form-control" id="inputCompany">
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">Role</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>Business Man</option>
+      <option>Business Woman</option>
+      <option>Tech consultant</option>
+      <option>Student</option>
+    </select>
+  </div>
+  
+  
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign Up</button>
+  </div>
+</form>
+
+
+
+
+
+</body>
+</html>
