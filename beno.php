51df4314f1247502fb03966dbc6636eb977cc12c
CBreezyJR <kazzyjr123@gmail.com>
2023-04-25 10:01:24 +0530

    up

---
+++ beno.php
@@ -0,0 +1,25 @@
+<style>
+    html{
+        height: 100%;
+        margin: 0px;
+        padding: 0px;
+    }
+
+    body{
+        margin: 0px;
+        padding: 0px;
+    }
+</style>
+
+<!DOCTYPE html>
+<html>
+    <body height="100%">
+    <script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.304/build/spline-viewer.js"></script>
+<spline-viewer hint loading-anim url="https://prod.spline.design/zmVeQ1XIhJQpy5il/scene.splinecode"></spline-viewer>
+    </body>
+</html>
+
+<script>
+
+    document.getElementById("logo").href="www.google.com";
+</script>
--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 
 
 
-
   
     <div class="align-self-center">
         <div class="row">
@@ -25,10 +24,10 @@
                 <div class="container fluid" style="width: 18rem;">
                     
                     <div class="card-body">
-                        <h1 class="card-title">BENO-TECH</h1>
+                        
                         <p class="card-text">LEARN,CONTRIBUTE AND CHALLENGE</p>
-                       <br> <a href="" class="btn btn-primary">Sign in</a>
-                        <a href="" class="btn btn-primary">Sign up</a>
+                       <br> <a href="login.php" class="btn btn-primary">Sign in</a>
+                        <a href="sign up.php" class="btn btn-primary">Sign up</a>
                     </div>
                 </div>
             </div>
@@ -46,6 +45,7 @@
 
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    
 </body>
 
 </html>
