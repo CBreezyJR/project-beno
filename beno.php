9949a2db32d572b1909bc1d2d7efb1374ac89409
CBreezyJR <kazzyjr123@gmail.com>
2023-04-26 10:11:40 +0530

    card box

--- beno.php
+++ beno.php
@@ -11,9 +11,48 @@
     }
 </style>
 
-<!DOCTYPE html>
-<html>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Beno</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>    
     <body height="100%">
+    <div class="align-self-center">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+            <div class="col">
+                
+                <div class="container fluid" style="width: 18rem;">
+                    
+                    <div class="">
+                        
+                        <p class="card-text">LEARN,CONTRIBUTE AND CHALLENGE</p>
+                       <br> <a href="login.php" class="btn btn-primary">Sign in</a>
+                        <a href="sign up.php" class="btn btn-primary">Sign up</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.304/build/spline-viewer.js"></script>
 <spline-viewer hint loading-anim url="https://prod.spline.design/zmVeQ1XIhJQpy5il/scene.splinecode"></spline-viewer>
     </body>
